76d94229db9443276491afc1843caaf11fad1ca5
ramieeee <rambotty@gmail.com>
2023-03-05 01:08:13 +0900

    Change api address

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  env: {
-    BASE_API_URL: process.env.BASE_API_URL || "http://localhost:3000",
-  },
 };
 
 module.exports = nextConfig;
