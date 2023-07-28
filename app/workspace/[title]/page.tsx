29618469d3acb85e389bc8a637ff8602e9be0d2f
ramieeee <rambotty@gmail.com>
2023-07-28 15:57:47 +0900

    Disable generateStaticParams()

--- app/workspace/[title]/page.tsx
+++ app/workspace/[title]/page.tsx
@@ -2,20 +2,20 @@ import ProjectClient from "./ProjectClient";
 import ProjectList from "@/interface/ProjectList";
 import { notFound } from "next/navigation";
 
-export async function generateStaticParams() {
-  const projectListData = await fetch(
-    `${process.env.BASE_API_URL}/api/projectlist`
-  );
-  const projectList = await projectListData.json();
+// export async function generateStaticParams() {
+//   const projectListData = await fetch(
+//     `${process.env.BASE_API_URL}/api/projectlist`
+//   );
+//   const projectList = await projectListData.json();
 
-  const list = projectList.resData.filter((project: ProjectList) => {
-    return project.url === "Confidential";
-  });
+//   const list = projectList.resData.filter((project: ProjectList) => {
+//     return project.url === "Confidential";
+//   });
 
-  return list.map((project: ProjectList) => ({
-    title: project.title,
-  }));
-}
+//   return list.map((project: ProjectList) => ({
+//     title: project.title,
+//   }));
+// }
 
 export default async function Project({
   params,
